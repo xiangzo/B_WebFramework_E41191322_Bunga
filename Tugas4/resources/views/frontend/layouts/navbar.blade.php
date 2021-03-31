5bd9dce1dbf83a6963718baaf5e558ecc0ff4afc
bunga <bungaelf@gmail.com>
2021-03-31 23:40:42 +0700

    revisi minggu 4

--- Tugas4/resources/views/frontend/layouts/navbar.blade.php
+++ Tugas4/resources/views/frontend/layouts/navbar.blade.php
@@ -1,8 +1,8 @@
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top">
+ <!-- ======= Header ======= -->
+ <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-      <a href="index.html" class="logo mr-auto"><img src="{{ asset('assets/img/logo.png')}}" alt="" class="img-fluid"></a>
+      <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
       <!-- Uncomment below if you prefer to use text as a logo -->
       <!-- <h1 class="logo mr-auto"><a href="index.html">Butterfly</a></h1> -->
 
--- Tugas4/resources/views/frontend/layouts/template.blade.php
+++ Tugas4/resources/views/frontend/layouts/template.blade.php
@@ -5,27 +5,27 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>{{ config('app.name') }}</title>
+  <title>{{ config('app.name')}}</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{ asset('frontend/assets/img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('frontend/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+  <link href="{{ asset('frontend/assets/img/favicon.png')}}" rel="icon">
+  <link href="{{ asset('frontend/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Dosis:300,400,500,,600,700,700i|Lato:300,300i,400,400i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{ asset('frontend/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('frontend/assets/vendor/icofont/icofont.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('frontend/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('frontend/assets/vendor/venobox/venobox.css') }}" rel="stylesheet">
-  <link href="{{ asset('frontend/assets/vendor/line-awesome/css/line-awesome.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('frontend/assets/vendor/owl.carousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('frontend/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{ asset('frontend/assets/vendor/icofont/icofont.min.css')}}" rel="stylesheet">
+  <link href="{{ asset('frontend/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{ asset('frontend/assets/vendor/venobox/venobox.css')}}" rel="stylesheet">
+  <link href="{{ asset('frontend/assets/vendor/line-awesome/css/line-awesome.min.css')}}" rel="stylesheet">
+  <link href="{{ asset('frontend/assets/vendor/owl.carousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="{{ asset('assets/css/style.css')}}" rel="stylesheet">
+  <link href="{{ asset('frontend/assets/css/style.css')}}" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Butterfly - v2.2.1
@@ -37,9 +37,9 @@
 
 <body>
 
-  @include('frontend/layouts.navbar')
+@include('frontend/layouts.navbar')
 
-  @yield('content')
+@yield('content')
 
   <!-- ======= Footer ======= -->
   <footer id="footer">
@@ -128,18 +128,18 @@
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="{{ asset('assets/vendor/jquery/jquery.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/jquery.easing/jquery.easing.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/php-email-form/validate.js')}}"></script>
-  <script src="{{ asset('assets/vendor/venobox/venobox.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/waypoints/jquery.waypoints.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/counterup/counterup.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/owl.carousel/owl.carousel.min.js')}}"></script>
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="{{ asset('assets/js/main.js')}}"></script>
+  <script src="assets/js/main.js"></script>
 
 </body>
 
